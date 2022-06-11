34c3bd1dd37c70539a392d6afe588f4f51efc0b9
alphak3y <alphamik80@gmail.com>
2022-06-11 21:43:45 +0100

    add comment to parseMacroCase

--- solmacs/types/macrocase.ts
+++ solmacs/types/macrocase.ts
@@ -11,5 +11,12 @@ export interface MacroCase {
 }
 
 export function parseMacroCase(caseExp: string, args: string[], startLine: number, endLine: number): MacroCase {
-    
+    // already passed in:
+    // - matchExp
+    // - args
+    // - startLine
+    // - endLine
+    //
+    // read through each line and find macroDefs
+    // output will be the final string after all inner macros have been resolved
 }
